2c7bb702c14ef8e2ba54629e1300f70c58caab5a
Calin Culianu <calin.culianu@gmail.com>
2025-05-24 21:47:02 +0300

    Revert "Imported some of the 2026 libauth vmb tests - 2026 "standard""
    This reverts commit f8ea334c3e1e5e311e91cd8c074951802339da55.
    
    We must do this because we will implement the other option for
    functions: OP_DEFINE and OP_INVOKE

--- src/test/libauth_tests.cpp
+++ src/test/libauth_tests.cpp
@@ -109,7 +109,7 @@ struct Upgrade12OverrideTestingSetup : Upgrade11OverrideTestingSetup {
     }
 };
 
-bool ran2022 = false, ran2023 = false, ran2025 = false, ran2026 = false;
+bool ran2022 = false, ran2023 = false, ran2025 = false;
 
 } // namespace
 
@@ -138,17 +138,10 @@ BOOST_FIXTURE_TEST_CASE(upgrade11_2025, Upgrade12OverrideTestingSetup) {
     ran2025 = true;
 }
 
-BOOST_FIXTURE_TEST_CASE(upgrade12_2026, Upgrade12OverrideTestingSetup) {
-    SetUpgrade11Active(true); // force Upgrade11 (vmlimts + bigint) to be active
-    SetUpgrade12Active(true); // force Upgrade 12 (p2s, relaxed limits, eval, loops)
-    RunTestPack("2026");
-    ran2026 = true;
-}
-
 // Precondition: This test *requires* that all Libauth test packs have previously completed as part of this
 // test_bitcoin run.
 BOOST_FIXTURE_TEST_CASE(test_lookup_table, TestingSetup) {
-    BOOST_REQUIRE(ran2022 && ran2023 && ran2025 && ran2026);
+    BOOST_REQUIRE(ran2022 && ran2023 && ran2025);
     LibauthTestingSetup::ProcessExpectedReasonsTable();
     LibauthTestingSetup::ProcessExpectedMetricsTable();
 }
